c14dee6b1a92f0a5a0d99d0043f045e8d230262d
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-01-13 17:43:25 +0700

    init

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+require('dotenv').config('../.env')
+import express from 'express'
+import router from './routes'
+
+const app = express()
+app.use(express.json())
+app.use(router)
+
+app.listen(4000, () => {
+  console.log('app is running at http://localhost:4000')
+})
